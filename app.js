b8334545a514922121c5122fd89ef2f44dd46753
Matt Haines <mhpahuta@gmail.com>
2022-07-29 10:51:01 -0700

    Add task add, delete, and filter functions

---
+++ app.js
@@ -0,0 +1,73 @@
+// UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+  form.addEventListener('submit', addTask);
+  // remove task event
+  taskList.addEventListener('click', removeTask);
+  // clear all tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  // filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Add task function
+function addTask(e) {
+  if (taskInput.value === '') { // handle empty input value
+    alert('Add a task');
+  }
+  // create li element
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  // create link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = `<i class="fa fa-remove"></i>`;
+  li.appendChild(link);
+  // append li to ul
+  taskList.appendChild(li);
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure you want to delete task?')) { // prompt for confirm
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear all Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // a more performative way, using a while loop:
+  while(taskList.firstChild) { // while there's still a first child
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(task => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
+}
